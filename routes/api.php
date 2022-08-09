65c96b45f528ac1692eb73cf914a6c3e9bee71ec
elkana.vinet <elkana.dimbiniaina@hotmail.com>
2022-08-09 09:32:01 +0300

    update route for zohosign test

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('zohosign')->group(function (){
+    Route::get('/', [MainController::class, 'callback'])->name('clients.zoho');
+});
