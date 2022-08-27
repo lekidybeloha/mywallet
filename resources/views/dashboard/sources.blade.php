fb3b6d73e236397c4707706d22ca731d6a8b44ea
Elkana Vinet <elkana.dimbiniaina@hotmail.com>
2022-08-27 10:14:55 +0300

    Finish CRUD on Sources

--- resources/views/dashboard/sources.blade.php
+++ resources/views/dashboard/sources.blade.php
@@ -15,6 +15,14 @@
                                 <td>
                                     {{ $one->nom }}
                                 </td>
+                                <td>
+                                    <a href="#" onclick="showModalEdit({{ $one->id }}, '{{ $one->nom }}')" type="button" class="btn btn-light px-5">
+                                        <i class="icon-pencil"></i>
+                                    </a>
+                                    <a href="#" onclick="showModal({{ $one->id }})" type="button" class="btn btn-outline-danger px-5">
+                                        <i class="icon-trash"></i>
+                                    </a>
+                                </td>
                             </tr>
                         @endforeach
                     @endif
@@ -53,5 +61,80 @@
                 </div>
             </div>
         </div>
+
+        <div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog bg-primary" role="document">
+                <div class="modal-content bg-light">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Suppression de revenu</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form method="post" action="{{ route('sources.delete') }}">
+                        @method('DELETE')
+                        <div class="modal-body">
+                            @csrf
+                            <span>
+                                Voulez vous vraiment supprimer cette source ?
+                                <br>
+                                En supprimant cette source, vous allez supprimer tous les revenu enregistré ainsi que toutes
+                                les historiques le concernant
+                            </span>
+                            <input type="hidden" name="id" value="" id="delete_id">
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-danger">Supprimer</button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog bg-primary" role="document">
+                <div class="modal-content bg-light">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Editer le nom d'une source</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form method="post" action="{{ route('sources.edit') }}">
+                        @method('PUT')
+                        <div class="modal-body">
+                            @csrf
+                            <span>
+                                Nom
+                            </span>
+                            <input type="text" name="nom" value="" id="edit_name">
+                            <input type="hidden" name="id" value="" id="edit_id">
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-danger">Mettre à jour</button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
+@endsection
+
+@section('scripts')
+    <script>
+        function showModal(id)
+        {
+            $('#delete_id').val(id);
+            $('#delete').modal('show')
+        }
+
+        function showModalEdit(id, nom)
+        {
+            $('#edit_id').val(id);
+            $('#edit_name').val(nom);
+            $('#edit').modal('show')
+        }
+    </script>
 @endsection
